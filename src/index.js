6ce64586070d260484e854cd4fdb9b3f7b562bc4
Vladislav Cheremisin <vladislav.cheremisin@artimpex.kz>
2020-10-06 14:30:23 +0300

    feat: brackets task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let arrLeft = [];
+    let arrRight = [];
+    str = str.split(``);
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      arrLeft.push(bracketsConfig[i][0]);
+      arrRight.push(bracketsConfig[i][1]);
+    }
+    for (let x = 0; x < str.length; x++) {
+      for (let y = 0; y < arrLeft.length; y++) {
+        if(str[x] === arrLeft[y] && str[x + 1] === arrRight[y]){
+          str.splice(x, 2);
+          x = -1;
+        }
+      }
+    }
+    return str.length === 0;
+  }
